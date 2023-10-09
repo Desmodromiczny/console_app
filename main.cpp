9de51ad8307a8de94fbb9cb78de8704a1d7208b4
Desmodromiczny <thxxesebb@gmail.com>
2023-10-09 15:49:59 +0200

    dodanie wyswietalcza godziny i daty

--- main.cpp
+++ main.cpp
@@ -8,23 +8,21 @@ void wyswietl_menu_glowne();
 
 std::vector<int> zwroc_date();
 std::vector<int> zwroc_godzine();
+void wyswietl_godzine(std::vector<int> godzina);
+void wyswietl_date(std::vector<int> data);
 
 
 int main() {
-    int i = 0;
-    int time = 2000;
-    /*
-    std::thread menu_display(wyswietl_menu_glowne);
-    menu_display.join();
-*/
+
+    //std::thread menu_display(wyswietl_menu_glowne);
+    //menu_display.join();
+
+
+
     std::vector<int> tablica = zwroc_date();
     std::vector<int> tablica2 = zwroc_godzine();
-    for(auto i : tablica){
-        std::cout << i << " ";
-    }
-    for(auto i : tablica2){
-        std::cout << i << " ";
-    }
+    wyswietl_godzine(zwroc_godzine());
+    wyswietl_date(zwroc_date());
 
     return 0;
 }
@@ -65,8 +63,35 @@ std::vector<int> zwroc_godzine(){
     return zwrot_godzina;
 };
 
+void wyswietl_godzine(std::vector<int> godzina){
+    int k = 0;
+    for(auto i : godzina){
+        if (k < godzina.size()) {
+            std::cout << i << ":";
+            k++;
+        }
+        else
+            std::cout << i;
+            k++;
+    }
+}
 
-void wyswietl_menu_glowne(){
+void wyswietl_date(std::vector<int> data){
+    int k = 0;
+    for(auto i : data){
+        if (k < data.size()) {
+            std::cout << i << ".";
+            k++;
+        }
+        else
+            std::cout << i;
+        k++;
+    }
+}
+
+
+
+void wyswietl_menu_glowne(std::vector<int> data, std::vector<int> godzina ){
     while(true) {
         std::cout << "*********************************************************************************" << std::endl;
         std::cout << "***                                                                           ***" << std::endl;
