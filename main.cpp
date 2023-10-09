2a27522242b0d80f7da4f9b343301ece77dede4d
Desmodromiczny <thxxesebb@gmail.com>
2023-10-09 17:13:10 +0200

    dodanie wyswietalcza godziny i daty bezpośrednio w funkcji wyswietlacza, nie działa prawidlowo - sprobowac wyrzucic do zewnetrzenej funkcji

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <chrono>
 #include <thread>
 #include <vector>
+#include <string>
 
-void wyswietl_menu_glowne();
+void wyswietl_menu_glowne(std::vector<int> data, std::vector<int> godzina);
 
 std::vector<int> zwroc_date();
 std::vector<int> zwroc_godzine();
@@ -14,8 +15,13 @@ void wyswietl_date(std::vector<int> data);
 
 int main() {
 
-    //std::thread menu_display(wyswietl_menu_glowne);
-    //menu_display.join();
+
+    std::thread menu_display(wyswietl_menu_glowne, zwroc_date(), zwroc_godzine());
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    menu_display.join();
+
+    //wyswietl_menu_glowne(zwroc_date(), zwroc_godzine());
+
 
 
 
@@ -91,8 +97,40 @@ void wyswietl_date(std::vector<int> data){
 
 
 
-void wyswietl_menu_glowne(std::vector<int> data, std::vector<int> godzina ){
+void wyswietl_menu_glowne(std::vector<int> data, std::vector<int> godzina){
     while(true) {
+        std::string wewn_data = [](auto data){
+            int k = 0;
+            std::string concat;
+            for(auto i : data){
+                if (k < data.size()) {
+                    concat += std::to_string(i);
+                    concat += ".";
+                    k++;
+                }
+                else
+                    concat += std::to_string(i);
+                    k++;
+            }
+            return concat;
+        }(data);
+        std::string wewn_godzina = [](auto data){
+            int k = 0;
+            std::string concat;
+            for(auto i : data){
+                if (k < data.size()) {
+                    concat += std::to_string(i);
+                    concat += ":";
+                    k++;
+                }
+                else
+                    concat += std::to_string(i);
+                k++;
+            }
+            return concat;
+        }(godzina);
+        std::cout << wewn_data << std::endl;
+        std::cout << wewn_godzina << std::endl;
         std::cout << "*********************************************************************************" << std::endl;
         std::cout << "***                                                                           ***" << std::endl;
         std::cout << "***                                                                           ***" << std::endl;
